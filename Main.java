033642b76d8a0a040b9425ca272487c38869cecd
yaren0y <yareny100@gmail.com>
2024-09-30 16:33:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+         int sum=0;
+
+         for (String s:args){
+
+             sum+=Integer.parseInt(s);
+         }
+        System.out.println(sum);
+
+
+        }
+    }
